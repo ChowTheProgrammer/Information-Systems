59bacd9329e5b164686c16c7bebbb606c60a5b0f
chowtheprogrammer <zhou267@purdue.edu>
2016-03-04 18:11:00 -0500

    Remove application code before due date

--- project1/data.sql
+++
@@ -1,3 +0,0 @@
-insert into Users values (users_sequence.nextval, 'Chow', 'Admin');
-insert into Users values (users_sequence.nextval, 'Jacob Lee', 'Student');
-insert into Users values (users_sequence.nextval, 'Greg Fredrickson', 'Faculty');
